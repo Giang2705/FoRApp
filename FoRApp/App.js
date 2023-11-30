34b50eb36d7e7c6a071d13f0fc26d5e0c006c372
metalboxxx <minhkhang09122003@gmail.com>
2023-11-30 18:55:55 +0700

    Basic chat - No function & No memory

--- FoRApp/App.js
+++ FoRApp/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import KengryTheBOT from './src/ChatBot';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello FoR</Text>
+      <KengryTheBOT></KengryTheBOT>
       <StatusBar style="auto" />
     </View>
   );
---
+++ FoRApp/src/ChatBot.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import OpenAI from "openai";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+
+
+
+const KengryTheBOT = () => {
+    const apiKey = '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'
+    const apiURL = 'https://api.openai.com/v1/chat/completions'
+    const [messagesData, setMessages] = useState([]);
+    const [userPrompt, setUserPrompt] = useState('');
+
+    const handleSend = async() => {
+        try {
+            const params = {
+                model: "gpt-3.5-turbo",
+                max_tokens : 70,
+                temperature: 0.5,
+                messages:  [{content: userPrompt, role:'user' }] 
+            }
+            const header = {
+                'Authorization': `Bearer ${apiKey}`,
+            }
+            const chatCompletion = await axios.post(apiURL,
+                params,
+                {headers: header})
+
+            const textResponse = chatCompletion.data.choices[0].message.content
+            setMessages([...messagesData,{role:'user', content: userPrompt },
+                                        {role : 'assistant', content: textResponse }])
+            setUserPrompt('');
+
+
+        } catch(e) {
+            console.log(e)
+        }
+
+
+    }
+
+    return(
+        <View>
+            <Text>Kengry the BOT</Text>
+            <FlatList
+                data={messagesData}
+                keyExtractor={(item,index)=>index.toString()}
+                renderItem={({item}) => (
+                    <View style = {{flexDirection:'row'}}>
+                        <Text>{item.role==='assistant'?'BOT: ':'USER: '}</Text>
+                        <Text>{item.content}</Text>
+                    
+                    </View>
+                )}>
+            </FlatList>
+            <TextInput
+                value= {userPrompt}
+                placeholder="Insert what you want to ask here"
+                onChangeText={text => setUserPrompt(text)}
+            ></TextInput>
+            <TouchableOpacity
+                onPress={handleSend}>
+                <Text>GO!</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default KengryTheBOT
+
+const styles = StyleSheet.create({
+
+})

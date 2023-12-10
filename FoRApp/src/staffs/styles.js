95e4512053d32a2058d559e2a45f4080c9f4b9b1
Quynh (May) Tran <s3872104@rmit.edu.vn>
2023-12-10 11:54:18 +0700

    image relative

--- FoRApp/src/staffs/styles.js
+++ FoRApp/src/staffs/styles.js
@@ -15,12 +15,21 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         alignSelf: "center",
         padding: 7,
-        marginTop: 25
+        marginTop: 55
     },
     editIcon: {
         position: "absolute",
-        marginTop: 25,
+        marginTop: 50,
         alignSelf: "flex-end",
+    },
+    section: {
+        fontFamily: "Montserrat-Bold"
+    },
+    foodImage: {
+        height: "35%",
+        width: "30%",
+        position: "relative",
+        marginTop: 15
     }
 })
 
